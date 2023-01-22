73dd289947a2202b3d4829e86503e6bb5a17b15e
Strahinja <strahinjav98@gmail.com>
2023-01-22 19:31:31 +0100

    code wars challenge

---
+++ createPhoneNumber.js
@@ -0,0 +1,4 @@
+function createPhoneNumber(numbers){
+    let str = "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+    return str
+  }
